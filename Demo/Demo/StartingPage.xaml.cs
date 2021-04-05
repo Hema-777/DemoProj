f1a515d0cd60322bb4dd571e45bbdb63ec341f4b
Aysha C M <ayshh.rasheed@gmail.com>
2021-04-05 19:24:41 +0530

    Add project files.

---
+++ Demo/Demo/StartingPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartingPage : ContentPage
+    {
+        public StartingPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ProfilePage());
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
